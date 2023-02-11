af2f476a8218d6c4e97841e477aa75c61a2afe05
vamsivardhanreddyN <vamsivardhan97@gmail.com>
2023-02-11 10:06:26 +0530

    added table ui and other files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
 import   ImportUI  from "./components/import/import";
 import DeleteUI from './components/delete/deleteUi';
+import Table from './components/Table/table';
+import Contacts from './components/Contacts/Contacts';
 
 function App() {
   
   return (
     <div className="App">
      
+     <Table/>
+     {/* <Contacts/> */}
+
     </div>
   );
 }
